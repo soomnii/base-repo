6e38d4af9207d94cfc41be25f985d5160f4bada6
soominlee <soominlee@soominleeui-MacBookPro.local>
2022-09-15 09:52:13 +0900

    add postgres & jpa related config & sample entity

---
+++ src/main/java/com/example/base/entity/Sample.java
@@ -0,0 +1,19 @@
+package com.example.base.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TB_SAMPLE")
+public class Sample {
+    @Id
+    @Column(name = "s_id")
+    String id;
+
+    @Column(name = "s_name")
+    String name;
+
+
+}
